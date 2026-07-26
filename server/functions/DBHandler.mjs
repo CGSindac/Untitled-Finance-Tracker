6f71a669b503246cb5b266e0acd7f123527642a6
Calix Gabriel <calixgabriel093@gmail.com>
2026-07-26 12:19:13 +0800

    Database Setup + Read & Write Functions

---
+++ server/functions/DBHandler.mjs
@@ -0,0 +1,72 @@
+import { write } from 'fs';
+import { readFile, writeFile } from 'fs/promises'
+
+const DB = "../db.json";
+
+async function getJSONData() {
+    try {
+        const rawJSON =  await readFile(DB, "utf-8");
+        const parsedJSON = JSON.parse(rawJSON);
+        return parsedJSON;
+    }catch (err) {
+        console.log(err);
+        return null;
+    }
+}
+
+function UpdateDB(newData){
+    try {
+        const newJSONData = JSON.stringify(newData, null, 4);
+        writeFile(DB, newJSONData, "utf-8");
+    } catch (err) { 
+        console.log(err);
+    }
+}
+
+async function createNewEntry( entryList, newEntry){
+    if (entryList == null) {
+        console.log("NO Database Found");
+        return false;
+    }
+
+    if (newEntry == null) {
+        console.log("Missing Fields");
+        return false;
+    }
+        
+    try {
+        newEntry = { 
+        id: entryList[entryList.length - 1].id + 1,
+        ...newEntry
+        }
+
+        return newEntry;
+    } catch (err) {
+        console.log(err);
+    }
+    
+}
+
+//============================TESTING===============================================//
+
+// const data =  await getJSONData();
+
+// if (data) {
+//     console.log(data);
+// }
+
+// const newData =  await createNewEntry(data, { 
+//     title : "Sample Journal Title #4",
+//     date : "26-7-2026",
+//     entry : "Lorem Ipsum..."
+// });
+
+
+// data.push(newData);
+
+
+
+
+// UpdateDB(data);
+
+
