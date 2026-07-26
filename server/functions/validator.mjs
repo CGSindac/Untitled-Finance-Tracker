addf34762676cf3c319ad017eeacbef3cca4053b
Calix Gabriel <calixgabriel093@gmail.com>
2026-07-26 16:44:04 +0800

    Validator to check updates/creates of entries

---
+++ server/functions/validator.mjs
@@ -0,0 +1,18 @@
+const validateEntry = (req, res, next) => {
+    const { body } = req;
+
+    // Check if all fields has value
+    if (!("title" in body && "entry" in body)) {
+        return res.send({mssg: "Error Missing Fields"});
+    }  
+
+    // Check if empty string
+    if (body.title.trim().length === 0) return res.status(400).send({mssg: "Error Empty Field"});
+    if (body.entry.trim().length === 0) return res.status(400).send({mssg: "Error Empty Field"});
+
+    next();
+} 
+
+export {
+    validateEntry
+}
